bd68e55d7fb0eac7605c36678d9067a751a79aed
andrew.lin <andrew_lin@gemteks.com>
2019-09-02 16:14:38 +0800

    add v1: MotionDetect_Count

---
+++ MotionDetect_Count/MotionDetect_Count.ino
@@ -0,0 +1,101 @@
+/*
+  HC-SR501 PIR Motion Detector setup
+  --Jumper Set         : use H (Repeat Trigger)
+  --Sensitivity Adjust : Anti C.W to the end (Low, about 3 meters)
+  --Time-delay  Adjust : Anti C.W to the end (Shorten, 5 sec)
+*/
+
+#define ledOFF()    digitalWrite(LED_BUILTIN, HIGH)
+#define ledON()     digitalWrite(LED_BUILTIN, LOW)
+
+/* in my ESP-01 module, LED_BUILTIN is D2,
+   so don't use D2 if you want to use ESP-01 onboard LED
+*/
+#define PIROUT_PIN          0
+#define DET_INTERVAL_MS     (1 * 1000)
+
+int sensor_data             = 0;
+uint16 detect_count         = 0;
+uint16 no_detect_count      = 0;
+uint16 max_no_detect_count  = 0;
+
+void _print2digit(int t) {
+  if (t < 10) Serial.print('0');
+  Serial.print(t);
+}
+
+void print_timestamp()
+{
+  unsigned long now_ms = millis() / 1000;
+  int t_sec = now_ms % 60;
+  int t_min = (now_ms / 60) % 60;
+  int t_hor = (now_ms / 3600) % 24;
+  _print2digit(t_hor); Serial.print(':');
+  _print2digit(t_min); Serial.print(':');
+  _print2digit(t_sec);
+}
+
+void setup() {
+  Serial.begin(115200);
+  delay(100);
+
+  /* HC-SR501 requires some time to acclimatize to the infrared energy in the room.
+     This takes 60 seconds when the sensor is first powered up.
+  */
+  Serial.println("\nHC-SR501 initializing...(60sec)");
+  delay(60 * 1000);
+
+  pinMode(PIROUT_PIN, INPUT);
+  pinMode(LED_BUILTIN, OUTPUT);
+  ledOFF();
+}
+
+void update_detect_counter() {
+  sensor_data = digitalRead(PIROUT_PIN);
+
+  if (sensor_data == HIGH)
+  {
+    //Serial.println("ON");
+    ledON();
+    no_detect_count = 0;
+    detect_count++;
+  }
+  else
+  {
+    //Serial.println("OFF");
+    ledOFF();
+    detect_count = 0;
+    no_detect_count++;
+
+    if (no_detect_count >= 65535) {
+      no_detect_count = 0;
+      max_no_detect_count = 0;
+    }
+
+    if (no_detect_count > max_no_detect_count) {
+      max_no_detect_count = no_detect_count;
+    }
+  }
+}
+
+void show_summary() {
+  print_timestamp();
+
+  Serial.print(" pv:");
+  Serial.print(sensor_data);
+  Serial.print(" dc:");
+  Serial.print(detect_count);
+  Serial.print("\tndc:");
+  Serial.print(no_detect_count);
+  Serial.print("\tMax(ndc):");
+  Serial.print(max_no_detect_count);
+  Serial.print("\t");
+  Serial.print((max_no_detect_count * DET_INTERVAL_MS) / 1000);
+  Serial.println("sec");
+}
+
+void loop() {
+  update_detect_counter();
+  show_summary();
+  delay(DET_INTERVAL_MS);
+}
--- README.md
+++ README.md
@@ -1,2 +1,37 @@
 # pir-motion-detector
 HC-SR501 PIR Motion Detector
+
+## MotionDetect_Count
+
+console output:
+
+
+	{timestamp} pv:{PIR value} dc:{detected count} ndc:{no-detected count} Max(ndc):{max of ndc} {time(sec)}
+	...
+	00:25:12 pv:0 dc:0	ndc:7	Max(ndc):42	42sec
+	00:25:13 pv:0 dc:0	ndc:8	Max(ndc):42	42sec
+	00:25:14 pv:0 dc:0	ndc:9	Max(ndc):42	42sec
+	00:25:15 pv:0 dc:0	ndc:10	Max(ndc):42	42sec
+	00:25:16 pv:0 dc:0	ndc:11	Max(ndc):42	42sec
+	00:25:17 pv:1 dc:1	ndc:0	Max(ndc):42	42sec
+	00:25:18 pv:1 dc:2	ndc:0	Max(ndc):42	42sec
+	00:25:19 pv:1 dc:3	ndc:0	Max(ndc):42	42sec
+	00:25:20 pv:0 dc:0	ndc:1	Max(ndc):42	42sec
+	00:25:21 pv:0 dc:0	ndc:2	Max(ndc):42	42sec
+	00:25:22 pv:0 dc:0	ndc:3	Max(ndc):42	42sec
+	00:25:23 pv:0 dc:0	ndc:4	Max(ndc):42	42sec
+	00:25:24 pv:1 dc:1	ndc:0	Max(ndc):42	42sec
+	00:25:25 pv:1 dc:2	ndc:0	Max(ndc):42	42sec
+	00:25:26 pv:1 dc:3	ndc:0	Max(ndc):42	42sec
+	00:25:27 pv:0 dc:0	ndc:1	Max(ndc):42	42sec
+	00:25:28 pv:0 dc:0	ndc:2	Max(ndc):42	42sec
+	00:25:29 pv:0 dc:0	ndc:3	Max(ndc):42	42sec
+	00:25:30 pv:0 dc:0	ndc:4	Max(ndc):42	42sec
+	00:25:31 pv:1 dc:1	ndc:0	Max(ndc):42	42sec
+	00:25:32 pv:1 dc:2	ndc:0	Max(ndc):42	42sec
+	00:25:33 pv:0 dc:0	ndc:1	Max(ndc):42	42sec
+	00:25:34 pv:0 dc:0	ndc:2	Max(ndc):42	42sec
+	00:25:35 pv:0 dc:0	ndc:3	Max(ndc):42	42sec
+	00:25:36 pv:0 dc:0	ndc:4	Max(ndc):42	42sec
+	00:25:37 pv:0 dc:0	ndc:5	Max(ndc):42	42sec
+
